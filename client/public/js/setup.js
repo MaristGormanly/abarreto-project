2c55fc1a9746d0f6ba4fd5f66b33818049894aee
Alemy Barreto <Alemy.Barreto1@marist.edu>
2019-03-25 19:32:41 -0400

    Adding changes to project

--- client/public/js/setup.js
+++ client/public/js/setup.js
@@ -3,3 +3,16 @@ document.addEventListener('keydown', function(event){
     window.location = "/mainmenu";
   }
 });
+
+function blink() {
+   var f = document.getElementById('msg');
+   setInterval(function() {
+      f.style.display = (f.style.display == 'none' ? '' : 'none');
+   }, 1000);
+}
+
+blink();
+
+function mainmenu(){
+  window.location = "/mainmenu";
+}
